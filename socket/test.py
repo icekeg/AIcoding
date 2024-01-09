de33eee1c8b207ff8e4cf680efec6ad9024f9804
alfredchen <alfredchen@tencent.com>
2024-01-09 22:48:03 +0800

    update

---
+++ socket/test.py
@@ -0,0 +1,9 @@
+def parse_url(url: str):
+    url = url.strip("/")
+    url = url.replace("0.0.0.0", "127.0.0.1")
+    if not url.startswith("http"):
+        url = f"http://{url}"
+    return url
+
+tt = parse_url("/127.0.0.1:8188")
+print(tt)
